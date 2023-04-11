eebbe79d884ea82486d6f90840d99c00efc94edc
DaxLarsen <124184307+DaxLarsen@users.noreply.github.com>
2023-04-11 09:55:12 -0600

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+'                                                ',
+'                              X X X        XXXX ',
+'                                      XX        ',
+'       XX  XX                      XX         X ',
+'     XXXX  XXXX      XXX   XX   XX    XXXXXXXXX ',
+'   XXXXX   XXXXXX         XXXX      XXXXXXXXXXX ',
+' XXXXXXXX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
+' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
+' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
+' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
+' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
