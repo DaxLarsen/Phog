757c751d206e4cf9dddff12609f237f0f1ddccf8
DaxLarsen <124184307+DaxLarsen@users.noreply.github.com>
2023-04-13 09:39:48 -0600

    Update level.py

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
+from player import Player
 
 class Level:
     def __init__(self,level_data,surface):
@@ -12,14 +13,41 @@ class Level:
         
     def setup_level(self,layout,):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index,row in enumerate(layout):
            for col_index,cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
                 if cell == 'X':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x,y),tile_size)
                     self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x,y + 32))
+                    self.player.add(player_sprite)
+
+    def scoll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - screen_width / 4 and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
 
     def run(self):
+        # level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        # player
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scoll_x()
