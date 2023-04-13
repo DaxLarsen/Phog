83325cd9eb83e1868c368aaae2b7ed1469c7dac5
DaxLarsen <124184307+DaxLarsen@users.noreply.github.com>
2023-04-13 09:38:52 -0600

    Update player.py

--- player.py
+++ player.py
@@ -1,2 +1,26 @@
 import pygame
 
+green = (141,221,101)
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pygame.Surface((32,32))
+        self.image.fill(green)
+        self.rect = self.image.get_rect(topleft = pos)
+        self.direction = pygame.math.Vector2(0,0)
+        self.speed = 8
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * self.speed
+
