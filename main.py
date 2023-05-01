5aa472874799b2b2c7296052f74c15485c61e762
Daxton Larsen <124184307+DaxLarsen@users.noreply.github.com>
2023-05-01 08:59:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, timer, random, sys
+import pygame, sys
 from settings import *
 from level import Level
 
@@ -8,21 +8,8 @@ screen = pygame.display.set_mode([screen_width, screen_height])
 clock = pygame.time.Clock()
 level = Level(level_map,screen)
 pygame.display.set_caption("Phog")
-icon = pygame.image.load("H:\CP2/phog/Phogicon.png")
+icon = pygame.image.load("H:\CP2/phog/character/Phogicon.png")
 pygame.display.set_icon(icon)
-pygame.font.init()
-font = pygame.font.Font('H:\CP2/phog/Gloria_Hallelujah/GloriaHallelujah-Regular.ttf', 48)
-
-
-"""for row_index,row in enumerate(layout):
-    for col_index,cell in enumerate(row):
-        x = col_index * tile_size
-        y = row_index * tile_size
-
-        if cell = 'X':
-            tile = Tile((x,y),tile_size)
-            self.tiles.add(tile)"""
-
 
 main = True
 
@@ -32,7 +19,7 @@ while main:
             pygame.quit()
             sys.exit()
         
-    #Create Backdrop Color
+    #Create Backdrop Color  
     screen.fill('black')
     level.run()
 
