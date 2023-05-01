bdedef8c6e831557e48138737a7958372dd08d46
Daxton Larsen <124184307+DaxLarsen@users.noreply.github.com>
2023-05-01 09:03:54 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 level_map = [
-'                                                ',
-'                              X X X        XXXX ',
-'                                      XX        ',
-'       XX  XX                      XX         X ',
-'     XXXX  XXXX      XXX   XX   XX    XXXXXXXXX ',
-'   XXXXX   XXXXXX         XXXX      XXXXXXXXXXX ',
-' XXXXXXXX  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
-' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
-' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
-' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ',
-' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ']
+'                                                                                                                            ',
+'                              X X X        X X X X X X                                                                      ',
+'                                      XX                                               XX   XX               XX           X ',
+'       XX  XX                      XX                                                                 X     XXXXXXXXXXXXXXX ',
+'   P XXXX  XXXX      XXX   XX   XX    XX   X     XXX   XXX   XXX   XXX   XXX    XXXX    X     XXX        XXXXXXXXXXXXXXXXXX ',
+'   XXXXXX  XXXXXX         XXXX      XXXXX    X                          X                        XX    X        XX          ',
+' XXXXX     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     XXXX            XXXX                             XX    X  XXX    XXX         ',
+' XX    XXX    XXXXXXXXXXXXXXXXXXXXXX       XXXXXXXXXX                         XXXXX XXXXXXXXXXXXX  XXX  XXXXX    XXX        ',
+' XX    XXXXX    XXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXX                XXXXXXXXXXXXX  XXX          XXXX XXXXXXX    X    XXXX ',
+' XX    XXXXXXX                   XXXXXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX  X  XXXXXXXX      XXXXXXXXX    XXXXXXX ',
+' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX     XXXXXX ']
 
 tile_size = 64
 screen_width = 1200
