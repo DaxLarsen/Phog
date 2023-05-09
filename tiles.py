48f8e8d24f76c1e800fdcd3aa033f72b252c9647
Daxton Larsen <124184307+DaxLarsen@users.noreply.github.com>
2023-05-09 10:04:07 -0600

    Update tiles.py

--- tiles.py
+++ tiles.py
@@ -9,3 +9,13 @@ class Tile(pygame.sprite.Sprite):
 
     def update(self,x_shift):
         self.rect.x += x_shift
+
+class Bounce(pygame.sprite.Sprite):
+    def __init__(self,pos,size):
+        super().__init__()
+        self.image = pygame.Surface((size,size))
+        self.image.fill('green')
+        self.rect = self.image.get_rect(topleft = pos)
+
+    def update(self,x_shift):
+        self.rect.x += x_shift
