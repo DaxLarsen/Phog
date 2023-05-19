8180256544edca6b8e5dc961ebdafba17c296ba6
Daxton Larsen <124184307+DaxLarsen@users.noreply.github.com>
2023-05-19 09:23:06 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,11 +6,12 @@ level_map = [
 '   P XXXX  XXX       BBB   XX   XX    XX   X     XXX   XXX   XXX   XXX   XXX    XXXX    X     XXX        XXXXXXXXXX      X                                                       ',
 '   XXXXXX  XXXXX         XXXX      XXXXX    X                          X                        XX    X         H       X          HX                             XXXXXXXXXXXXX  ',
 ' XXXXX     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     XXXX            XXXX                 XXXXX       XX    X  XXX    XXX                                                XXXXXXXXXX    ',
-' XX     XX    XXXXXXXXXXXXXXXXXXXXXX       XXXXXXXXXX                         XXXXX HHHHHXXXXXXXX  XXX  XXXXX    XXX   X                       X                     XXXX        ',
+' XX     XX    XXXXXXXXXXXXXXXXXXXXXX       XXXXXXXXXX                         XXXXX  HHH XXXXXXXX  XXX  XXXXX    XXX   X                       X                     XXXX        ',
 ' XX    XXXXX    XXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXX                XXXXXXXXXXXXX  XXX          XXXX XXXXXXX    X    XXXBB     X        X                                      ',
-' XX    XXXXXXX  HHHHHHHHHHHHHH   XXXXXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX  X  XXXXXXXX      XXXXXXXXX    XXXXXXX           H                                          ',
+' XX    XXXXXXX   HHHHHHHHHHHH   XXXXXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX  X  XXXXXXXX      XXXXXXXXX    XXXXXXX           H                                          ',
 ' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX     XXXXXX                                                      ']
 
 tile_size = 64
 screen_width = 1200
 screen_height = len(level_map) * tile_size
+
