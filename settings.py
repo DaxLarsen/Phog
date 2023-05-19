34a3a91fab72c44308ceb65e459bab0afacf1669
Tyler Gardner <123663346+tg050404@users.noreply.github.com>
2023-05-19 08:37:58 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 level_map = [
-'                                                                                                                             ',
-'                              X X X        X X X X X X                                                                       ',
-'                                      XX                                               XX   XX               XX           X  ',
-'       XX  XX                      XX                                                                 X     XXXXXXXXXXXXXXX  ',
-'   P XXXX  XXX       BBB   XX   XX    XX   X     XXX   XXX   XXX   XXX   XXX    XXXX    X     XXX        XXXXXXXXXXXXXXXXXX  ',
-'   XXXXXX  XXXXX          XXXX      XXXXX    X                          X                        XX    X         H           ',
-' XXXXX     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     XXXX            XXXX                 XXXXX       XX    X  XXX    XXX          ',
-' XX     XX    XXXXXXXXXXXXXXXXXXXXXX       XXXXXXXXXX                         XXXXX HHHHHXXXXXXXX  XXX  XXXXX    XXX   X     ',
-' XX    XXXXX    XXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXX                XXXXXXXXXXXXX  XXX          XXXX XXXXXXX    X    XXXBB ',
-' XX    XXXXXXX  HHHHHHHHHHHHHH   XXXXXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX  X  XXXXXXXX      XXXXXXXXX    XXXXXXX  ',
-' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX     XXXXXX  ']
+'                                                                                                                                                   HHHHHH            ',
+'                              X X X        X X X X X X                                                                  X                  H  H    HHHHHH            ',
+'                                      XX                                               XX   XX               XX          X               X         HHHHHH            ',
+'       XX  XX                      XX                                                                 X     XXXXXXX  XXXXXX                        XXXXXX            ',
+'   P XXXX  XXX       BBB   XX   XX    XX   X     XXX   XXX   XXX   XXX   XXX    XXXX    X     XXX        XXXXXXXXXX      X                                           ',
+'   XXXXXX  XXXXX         XXXX      XXXXX    X                          X                        XX    X         H       X          HX                                ',
+' XXXXX     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     XXXX            XXXX                 XXXXX       XX    X  XXX    XXX                                                  ',
+' XX     XX    XXXXXXXXXXXXXXXXXXXXXX       XXXXXXXXXX                         XXXXX HHHHHXXXXXXXX  XXX  XXXXX    XXX   X                       X                     ',
+' XX    XXXXX    XXXXXXXXXXXXXX       XXXXXXXXXXXXXXXXX                XXXXXXXXXXXXX  XXX          XXXX XXXXXXX    X    XXXBB     X        X                          ',
+' XX    XXXXXXX  HHHHHHHHHHHHHH   XXXXXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX  X  XXXXXXXX      XXXXXXXXX    XXXXXXX           H                              ',
+' XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX   XXXXXXXXXXXXXXXXXXXXXXXX     XXXXXX                                          ']
 
 tile_size = 64
 screen_width = 1200
